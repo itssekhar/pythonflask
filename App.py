fbc15d2ba8cf3dc037f207146e61c980d3af8e84
itssekhar <itssekhar511@gmail.com>
2019-09-30 11:32:36 -0500

    BuildAppusingflask

--- App.py
+++ App.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 
 def index():
     return 'This is the homepage'
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
